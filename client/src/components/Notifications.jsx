8542d5f72c24f963c124953d7a9575ada9547469
shivam910 <77653717+shivam910@users.noreply.github.com>
2021-10-18 11:39:14 +0530

    update

--- client/src/components/Notifications.jsx
+++ client/src/components/Notifications.jsx
@@ -1,6 +1,6 @@
 import { Button } from '@material-ui/core';
 import React, {useContext} from 'react';
-import SocketContext from "SocketContext.js";
+import { SocketContext } from "../SocketContext";
 
 const Notifications = () => {
 
@@ -9,7 +9,7 @@ const Notifications = () => {
     return (
         <>
             {call.isReceivingCall && !callAccepted &&  (
-                <div style= {{display: 'flex', justifyContent = 'space-around'}}>
+                <div style= {{display: 'flex', justifyContent : 'space-around'}}>
                     <h1>{call.name} is calling</h1>
                     <Button variant='contained' color='primary' onClick={answerCall}>
                         Answer
--- client/src/components/Options.jsx
+++ client/src/components/Options.jsx
@@ -1,58 +1,118 @@
-import React, { useContext, useState } from 'react';
-import { Button, TextField, Grid, Typography, Container, Paper } from "@material-ui/core";
-import { makeStyles } from '@material-ui/core/styles';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
-import { Assignment, Phone, PhoneDisabled} from "@material-ui/icons";
-import { SocketContext } from '../SocketContext';
+import React, { useContext, useState } from "react";
+import {
+  Button,
+  TextField,
+  Grid,
+  Typography,
+  Container,
+  Paper,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { Assignment, Phone, PhoneDisabled } from "@material-ui/icons";
+import { SocketContext } from "../SocketContext";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      display: 'flex',
-      flexDirection: 'column',
+  root: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  gridContainer: {
+    width: "100%",
+    [theme.breakpoints.down("xs")]: {
+      flexDirection: "column",
     },
-    gridContainer: {
-      width: '100%',
-      [theme.breakpoints.down('xs')]: {
-        flexDirection: 'column',
-      },
+  },
+  container: {
+    width: "600px",
+    margin: "35px 0",
+    padding: 0,
+    [theme.breakpoints.down("xs")]: {
+      width: "80%",
     },
-    container: {
-      width: '600px',
-      margin: '35px 0',
-      padding: 0,
-      [theme.breakpoints.down('xs')]: {
-        width: '80%',
-      },
-    },
-    margin: {
-      marginTop: 20,
-    },
-    padding: {
-      padding: 20,
-    },
-    paper: {
-      padding: '10px 20px',
-      border: '2px solid black',
-    },
-  }));
+  },
+  margin: {
+    marginTop: 20,
+  },
+  padding: {
+    padding: 20,
+  },
+  paper: {
+    padding: "10px 20px",
+    border: "2px solid black",
+  },
+}));
 
-const Options = ( {children} ) => {
-    const {me, callAccepted, name, setName, callEnded, leaveCall, callUser} = useContext( SocketContext );
-    const [ idToCall, setIdToCall] = useState('');
-    const classes = useStyles();
-    return (
-        <Container className= {classes.container}>
-            <Paper elevation= {10} className={classes.paper}>
-                <form className={classes.root} noValidate autoCapitalize="off">
-                    <Grid container className={classes.gridContainer} >
-                        <Grid item xs={12} md={6} className={classes.padding}>
-                            <Typography gutterBottom variant="h6">Account INFO</Typography>
-                        </Grid>
-                    </Grid>
-                </form>
-            </Paper>
-            {children}
-        </Container>
-    )}
+const Options = ({ children }) => {
+  const { me, callAccepted, name, setName, callEnded, leaveCall, callUser } =
+    useContext(SocketContext);
+  const [idToCall, setIdToCall] = useState("");
+  const classes = useStyles();
+  return (
+    <Container className={classes.container}>
+      <Paper elevation={10} className={classes.paper}>
+        <form className={classes.root} noValidate autoCapitalize="off">
+          <Grid container className={classes.gridContainer}>
+            <Grid item xs={12} md={6} className={classes.padding}>
+              <Typography gutterBottom variant="h6">
+                Account INFO
+              </Typography>
+              <TextField
+                label="Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                fullWidth
+              />
+              <CopyToClipboard text={me} className={classes.margin}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                  startIcon={<Assignment FontFace="large" />}
+                >
+                  Copy Your Id
+                </Button>
+              </CopyToClipboard>
+            </Grid>
+            <Grid item xs={12} md={6} className={classes.padding}>
+              <Typography gutterBottom variant="h6">
+                Make A Call
+              </Typography>
+              <TextField
+                label="Id to Call"
+                value={idToCall}
+                onChange={(e) => setName(e.target.value)}
+                fullWidth
+              />
+              {callAccepted && !callEnded ? (
+                <Button
+                variant="contained"
+                color="secondary"
+                fullWidth
+                startIcon={<PhoneDisabled FontFace="large" />}
+                fullWidth
+                onClick={leaveCall}
+                className = {classes.margin}>Hang Up</Button>
+              ) : (
+                <Button
+                variant="contained"
+                color="secondary"
+                fullWidth
+                startIcon={<Phone FontFace="large" />}
+                fullWidth
+                onClick={ () => callUser(idToCall) }
+                className = {classes.margin}
+                >
+                  Connect to Tutor
+                </Button>
+              )}
+            </Grid>
+          </Grid>
+        </form>
+      {children}
+      </Paper>
+    </Container>
+  );
+};
 
 export default Options;
